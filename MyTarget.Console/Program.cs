bd71bc841d290b70836cd9893905e308b912d6e4
tiagosp1982 <129423722+tiagosp1982@users.noreply.github.com>
2023-06-27 17:35:15 -0300

    Add files via upload

---
+++ MyTarget.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyTarget.Application.Commands;
+using MyTarget.Application.Interfaces;
+using MyTarget.Application.Services;
+using MyTarget.Infra.IoC;
+
+namespace MyTarget.Console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+            serviceProvider.GetService<ExecuteCommand>().Execute();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServices();
+        }
+    }
+}
