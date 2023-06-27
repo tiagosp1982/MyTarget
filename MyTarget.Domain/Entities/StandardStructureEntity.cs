48bd3ff9e720203c641df8f604031a01903ce0d9
tiagosp1982 <129423722+tiagosp1982@users.noreply.github.com>
2023-06-27 17:38:33 -0300

    Add files via upload

---
+++ MyTarget.Domain/Entities/StandardStructureEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace MyTarget.Domain.Entities
+{
+    public class StandardStructureEntity
+    {
+        public int standardNumber { get; set; }
+    }
+}
---
+++ MyTarget.Domain/Interfaces/IAmountTensBetRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTarget.Domain.Interfaces
+{
+    public interface IAmountTensBetRepository
+    {
+        List<int> GetAll();
+    }
+}
